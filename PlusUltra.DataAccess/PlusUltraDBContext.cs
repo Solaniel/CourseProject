194c859ae494de452a23c89c95dca30237c99220
Branimir Petkov <yellowcow@abv.bg>
2018-05-23 00:15:56 +0300

    Fixing Database Context

--- PlusUltra.DataAccess/PlusUltraDBContext.cs
+++ PlusUltra.DataAccess/PlusUltraDBContext.cs
@@ -1,6 +1,10 @@
-﻿using 
-using System.Data.Entity;
-
+﻿using System.Data.Entity;
+using PlusUltra.DataAccess;
+using System;
+using PlusUltraDB.Entities;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace PlusUltraDB
 {
@@ -12,6 +16,13 @@ namespace PlusUltraDB
         }
 
         public DbSet<Game> Games { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Organization> Organizations { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder) //method that prevents table names to be pluralized
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
 
     }
 }
